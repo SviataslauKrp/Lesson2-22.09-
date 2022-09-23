f57ac653ffedcf1a51304c61729a511f712c91a7
SviataslauKrp <kirpik.svtatoslav@yandex.ru>
2022-09-23 21:15:42 +0300

    corrected

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        String name = console.nextLine();
-        System.out.println("Hello " + name);
+        String userName = console.nextLine();
+        System.out.println("Hello " + userName);
+        console.close();
     }
 }
